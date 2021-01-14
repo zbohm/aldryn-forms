c8d37315383ef66378e12ee38492d0c887b853aa
Zdeněk Böhm <zdenek.bohm@seznam.cz>
2021-01-14 20:48:01 +0100

    Add default_app_config apps.AldrynForms.

--- aldryn_forms/__init__.py
+++ aldryn_forms/__init__.py
@@ -1,2 +1,4 @@
 # -*- coding: utf-8 -*-
 __version__ = '5.0.1'
+
+default_app_config = 'aldryn_forms.apps.AldrynForms'
---
+++ aldryn_forms/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AldrynForms(AppConfig):
+    name = 'aldryn_forms'
+    verbose_name = 'Aldryn forms'
